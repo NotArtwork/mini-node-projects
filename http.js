f596494756bad9857bcd0aa2e82092eb56ffbdae
NotArtwork <AntArce3@gmail.com>
2022-11-07 15:33:42 -0500

    added block patterns

--- http.js
+++ http.js
@@ -20,3 +20,31 @@ server.listen(5001)
 
 
 console.log('stuff')
+
+
+// Syncronous (DO NOT)
+
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+
+//     if (req.url === '/') {
+//         res.end('Home Page')
+//     }
+
+//     if (req.url === '/about') {
+//         res.end('About Page')
+//         for (let i = 0; i < 1000; i++) {
+//             for (let j = 0; j < 1000; j++) {
+//                 console.log(i, j)
+//             }
+//         }
+//     }
+
+//     res.end('Error Page')
+
+// })
+
+// server.listen(5001, () => {
+//     console.log('Server Listening on Port 5001')
+// })
