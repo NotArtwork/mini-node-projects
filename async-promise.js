45e8c361a18a8cfde8b6dd0d570ea587d73720df
NotArtwork <AntArce3@gmail.com>
2022-11-09 13:39:01 -0500

    fixed async

--- async-promise.js
+++ async-promise.js
@@ -1,4 +1,4 @@
-const { readFile, writeFile } = require('fs');
+const { readFile, writeFile } = require('fs').promises
 const util = require('util')
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
@@ -7,10 +7,10 @@ const writeFilePromise = util.promisify(writeFile)
 
 const start = async () => {
     try {
-        const first = await readFilePromise('./content/first.txt', 'utf8')
-        const second = await readFilePromise('./content/second.txt', 'utf8')
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
 
-        await writeFilePromise('./content/result-mind-grenade.txt', `THIS IS AWESOME :) ${first}, ${second}`)
+        await writeFile('./content/result-mind-grenade.txt', `THIS IS AWESOME :) ${first}, ${second}`)
 
         console.log(first, second)
     } catch (error) {
