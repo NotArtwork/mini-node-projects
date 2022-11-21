3d0b932ff06a73b1cd6d284dcaebfc3b9add883f
NotArtwork <AntArce3@gmail.com>
2022-11-21 16:13:25 -0500

    added optional page renderings

--- express_stuff/app.js
+++ express_stuff/app.js
@@ -1,8 +1,30 @@
 const http = require('http')
+const { url } = require('inspector')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, )
-    console.log('User hit the server')
+
+    const url = req.url
+
+    if (url === '/') {
+
+        res.writeHead(200, {'content-type' : 'text/html'})
+        console.log('User hit the server')
+        res.end('<h1> home page </h1>')
+
+    }
+    else if (url ==='/about') {
+
+        res.writeHead(200, { 'content-type': 'text/html' })
+        console.log('User hit the server')
+        res.end('<h1> About Page </h1>')
+
+    }
+    else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        console.log('User hit the server')
+        res.end('<h1> Page not found! </h1>')
+    }
+
 })
 
 server.listen(5001)
